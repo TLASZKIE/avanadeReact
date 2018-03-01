7870cba574ae32dc6acbe77f4bea350831d4368b
Tomek Łaszkiewicz <tomasz.laszkiewicz@avanade.com>
2018-03-01 10:29:36 +0100

    ProductList

---
+++ js/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// We use the model ...
+var products = productsModel()
+// ... and we render using our view
+renderProducts(document.querySelector('#products'), products)
---
+++ js/products.js
@@ -0,0 +1,32 @@
+'use strict'
+var products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    img: 'https://xpla.org/ext/lorempixel/250/250/technics/1/',
+    desc: 'Cool first product'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    img: 'https://xpla.org/ext/lorempixel/250/250/technics/1/',
+    desc: 'Cool second product'
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    img: 'https://xpla.org/ext/lorempixel/250/250/technics/1/',
+    desc: 'Cool third product'
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    img: 'https://xpla.org/ext/lorempixel/250/250/technics/1/',
+    desc: 'Cool fourth product'
+  }
+]
+
+//3/ We set our model outside
+function productsModel() {
+  return products
+}
