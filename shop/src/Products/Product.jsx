d9c3dee92849bab14ed1a3a00c7bb6f6689320eb
Tomek Łaszkiewicz <tomasz.laszkiewicz@avanade.com>
2018-03-02 10:37:27 +0100

    sorting

--- shop/src/Products/Product.jsx
+++ shop/src/Products/Product.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function Product(props) {
-  const { img, name, desc, specialOffer, toggle, onToggle } = props
+  const { img, name, price, desc, specialOffer, toggle, onToggle } = props
 
   const specialClass = specialOffer ? 'special' : ''
 
   return (
-    <div className={specialClass}>
-      <img src={img} alt={name} onClick={onToggle} />
+    <div className={`products-product ${specialClass}`}>
+      <img src={img} alt={name} onClick={onToggle} width="100" height="100" />
       <div>{name}</div>
+      <div>{price}</div>
       {toggle ? <div>{desc}</div> : null}
     </div>
   )
--- shop/src/Products/ProductsContainer.jsx
+++ shop/src/Products/ProductsContainer.jsx
@@ -3,7 +3,9 @@ import ProductsList from './ProductsList'
 
 export default class ProductsContainer extends Component {
   state = {
-    products: []
+    products: [],
+    sorting: 'asc',
+    filter: ''
   }
 
   componentDidMount() {
@@ -12,11 +14,35 @@ export default class ProductsContainer extends Component {
       .then(products => this.setState({ products }))
   }
 
+  handleSort = () => {
+    this.state.sorting === 'asc'
+      ? this.setState({ sorting: 'desc' })
+      : this.setState({ sorting: 'asc' })
+  }
+
   render() {
+    const { products, sorting, filter } = this.state
+
+    const filteredProducts =
+      filter === '' ? products : products.filter(p => p.name.includes(filter))
+
+    const sortedProducts =
+      sorting === 'asc'
+        ? filteredProducts.concat().sort((a, b) => a.price < b.price)
+        : filteredProducts.concat().sort((a, b) => a.price > b.price)
+
+    console.log(sorting)
+    console.log(sortedProducts)
+
     return (
       <div className="ProductsContainer">
-        <ProductsList products={this.state.products} />
+        <button onClick={this.handleSort}>{sorting}</button>
+        <ProductsList products={sortedProducts} />
       </div>
     )
+  }
+
+  compare(a, b) {
+    return a - b
   }
 }
